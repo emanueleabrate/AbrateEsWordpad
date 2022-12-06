7cfd931863c7a32eeb4326bf795d6944770c4ad4
emanueleabrate <e.abrate.2192@vallauri.edu>
2022-12-06 11:44:49 +0100

    Modificato nome form

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace AbrateEsWordpad
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmWordpad());
         }
     }
 }
--- frmWordpad.cs
+++ frmWordpad.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AbrateEsWordpad
 {
-    public partial class Form1 : Form
+    public partial class frmWordpad : Form
     {
-        public Form1()
+        public frmWordpad()
         {
             InitializeComponent();
         }
